8e227e64c4f58edf4705106b8096f6afc06cfa41
GutMaria <gootmaria@gmail.com>
2023-10-25 16:37:28 +0300

    test Class Api Service

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,19 +4,80 @@ axios.defaults.headers.common['x-api-key'] =
   'live_nbDpU1qcjOjwGVJZCuTkD0kncD8qZSwjCGrP3KTDX7sI9dlwuKvRY9fu8uNEUaPp';
 
 const URL = 'https://api.thecatapi.com/v1/';
+const loader = document.querySelector('.loader');
+const loaderError = document.querySelector('.error');
 
-function fetchBreeds() {
-  return axios
-    .get(`${URL}breeds`)
-    .then(response => response.data)
-    .catch(error => console.error(error));
+// function fetchBreeds() {
+//   loader.hidden = false;
+//   return axios
+//     .get(`${URL}breeds`)
+//     .then(response => {
+//       loader.hidden = true;
+//       return response.data;
+//     })
+//     .catch(() => {
+//       loader.hidden = true;
+//       loaderError.hidden = false;
+//       return [];
+//       // ----------------Чи треба повертати пустий масив ????
+//     });
+// }
+
+// function fetchCatByBreed(breedId) {
+//   loader.hidden = false;
+//   loaderError.hidden = true;
+
+//   return axios
+//     .get(`${URL}images123/search?breed_ids=${breedId}`)
+//     .then(response => {
+//       loader.hidden = true;
+//       return response.data;
+//     })
+//     .catch(() => {
+//       loader.hidden = true;
+//       loaderError.hidden = false;
+//       // console.error(error);
+//       return [];
+//     });
+// }
+
+// export { fetchBreeds, fetchCatByBreed };
+
+// Пробую створити класс для роботи з АРІ-----------------
+
+export default class ApiService {
+  constructor() {}
+
+  fetchBreeds() {
+    loader.hidden = false;
+    return axios
+      .get(`${URL}breeds`)
+      .then(response => {
+        loader.hidden = true;
+        return response.data;
+      })
+      .catch(() => {
+        loader.hidden = true;
+        loaderError.hidden = false;
+        return [];
+      });
+  }
+
+  fetchCatByBreed(breedId) {
+    loader.hidden = false;
+    loaderError.hidden = true;
+
+    return axios
+      .get(`${URL}images/search?breed_ids=${breedId}`)
+      .then(response => {
+        loader.hidden = true;
+        return response.data;
+      })
+      .catch(() => {
+        loader.hidden = true;
+        loaderError.hidden = false;
+        // console.error(error);
+        return [];
+      });
+  }
 }
-
-function fetchCatByBreed(breedId) {
-  return axios
-    .get(`${URL}images/search?breed_ids=${breedId}`)
-    .then(response => response.data)
-    .catch(error => console.error(error));
-}
-
-export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,73 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+// import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const selectCat = document.querySelector('.breed-select');
 const catCard = document.querySelector('.cat-info');
+// const loader = document.querySelector('.loader');
+
+// При завантажені сторінки, запит на сервер за колекцією порід, і створення списку в Select
+// fetchBreeds().then(data => createOptions(data));
+
+// selectCat.addEventListener('change', selectBreed);
+
+// function createOptions(arr) {
+//   // loader.hidden = true;
+//   selectCat.hidden = false;
+//   const options = arr.map(({ id, name }) => {
+//     return `<option value=${id}>${name}</option>`;
+//   });
+
+//   selectCat.innerHTML = options.join('');
+// }
+
+// function selectBreed(evt) {
+//   catCard.innerHTML = '';
+//   // loader.hidden = false;
+//   let breedId = evt.currentTarget.value;
+//   fetchCatByBreed(breedId).then(data => renderCatCard(data[0]));
+// }
+
+// function renderCatCard({ url, breeds }) {
+//   const temperament = breeds[0].temperament;
+//   const name = breeds[0].name;
+//   const description = breeds[0].description;
+
+//   const catInfo = `<img src="${url}" alt="${name}" width=400/>
+//   <div>
+//   <h2>${name}</h2>
+//   <P>${description}</P>
+//   <p>Temperament: ${temperament}</p>
+//   </div>`;
+
+//   catCard.innerHTML = catInfo;
+// }
+
+// ------------------------Пробую створити класс для роботи з АРІ---------------------------
+
+import ApiService from './cat-api.js';
+// створюємо екземпляр класу
+const ApiService = new ApiService();
+console.log(ApiService);
+ApiService.fetchBreeds();
+// -----------Застрягла------------
+
+// ApiService.fetchBreeds().then(data => createOptions(data));
+
+// selectCat.addEventListener('change', selectBreed);
 
 function createOptions(arr) {
-  const options = arr.map(
-    ({ id, name }) => `<option value=${id}>${name}</option>`
-  );
+  // loader.hidden = true;
+  selectCat.hidden = false;
+  const options = arr.map(({ id, name }) => {
+    return `<option value=${id}>${name}</option>`;
+  });
 
   selectCat.innerHTML = options.join('');
 }
 
-fetchBreeds().then(data => createOptions(data));
-
-let breedId;
-
-selectCat.addEventListener('change', selectBreed);
-
 function selectBreed(evt) {
-  breedId = evt.currentTarget.value;
+  catCard.innerHTML = '';
+  // loader.hidden = false;
+  let breedId = evt.currentTarget.value;
   fetchCatByBreed(breedId).then(data => renderCatCard(data[0]));
 }
 
@@ -29,10 +78,10 @@ function renderCatCard({ url, breeds }) {
 
   const catInfo = `<img src="${url}" alt="${name}" width=400/>
   <div>
-<h2>${name}</h2>
-<P>${description}</P>
-<p>Temperament:${temperament}</p>
-</div>`;
+  <h2>${name}</h2>
+  <P>${description}</P>
+  <p>Temperament: ${temperament}</p>
+  </div>`;
 
   catCard.innerHTML = catInfo;
 }
